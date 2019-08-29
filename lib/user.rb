1eb4a5cf0a9dd320dc3ecd168dac66e972fa08d0
RMaughan93 <rhys.maughan93@gmail.com>
2019-08-29 14:54:31 +0100

    added user-spec

--- lib/user.rb
+++ lib/user.rb
@@ -5,7 +5,8 @@ class User
     @listings = set_listings
   end
 
-def set_listings
-  PG.connect()
-  connection.exec('select * from properties where user_id = #{id}')
+  def set_listings
+    PG.connect()
+    connection.exec('select * from properties where user_id = #{id}')
+  end
 end
---
+++ spec/user_spec.rb
@@ -0,0 +1,15 @@
+require 'user'
+
+describe User do
+
+  describe '.create' do
+    
+    it 'adds user to database' do
+      user = User.create(email: "h@h.h", password: "12345")
+      expect(user.email).to eq("h@h.h")
+      expect(user.id.to_i).to be_a_kind_of(Numeric)
+    end
+    
+  end
+
+end
