3cd7bb0cdd7ac50a8a9c8d58fe0f2ca7186800d5
RMaughan93 <rhys.maughan93@gmail.com>
2019-08-29 14:52:46 +0100

    added columns to sql table in migration file

--- db/migrations/02_create_properties.sql
+++ db/migrations/02_create_properties.sql
@@ -1,2 +1,1 @@
-
-CREATE TABLE properties(id SERIAL PRIMARY KEY, title  VARCHAR(60), city VARCHAR(60), user_id SERIAL REFERENCES user(id));
+CREATE TABLE properties(id SERIAL PRIMARY KEY, title VARCHAR(60), city VARCHAR(60), pricepernight INTEGER, details VARCHAR(240), user_id int, FOREIGN KEY (user_id) REFERENCES users(id));
--- lib/listing.rb
+++ lib/listing.rb
@@ -1,6 +1,9 @@
 require_relative 'database_connection'
+require_relative '../database_connection_setup'
 
 class Listing
+   
+  DEFAULT_PRICE = 35
 
   attr_reader :id, :title, :city, :user_id
 
@@ -9,18 +12,20 @@ class Listing
     @title = title
     @city = city
     @user_id = user_id
+    @details = ""
+    @price_per_night = DEFAULT_PRICE
   end
 
   def self.create(title:, city:)
-    DatabaseConnection.setup('makers_bnb')
+    # DatabaseConnection.setup('makers_bnb')
     result = DatabaseConnection.query("INSERT INTO properties (title, city) VALUES('#{title}', '#{city}') RETURNING id, title, city, user_id")
     Listing.new(id: result[0]['id'], title: result[0]['title'], city: result[0]['city'], user_id: result[0]['user_id'])
   end
 
-  # def self.edit(title:, city:)
-  #   DatabaseConnection.setup('makers_bnb')
-  #   DatabaseConnection.query("insert into properties (title, city) values ('#{title}', '#{city}' where id = #{@id}")
-  # end
+  def self.edit(title:, city:, description:)
+    # DatabaseConnection.setup('makers_bnb')
+    DatabaseConnection.query("insert into properties (title, city, description) values ('#{title}', '#{city}', #{description} where id = #{@id}")
+  end
 
   def self.all
     DatabaseConnection.setup('makers_bnb')
